13fdf4526cbb957e19e515f2c8e1e33ae2d38bc6
Magnus Åsrud <magnusasrud92@gmail.com>
2023-12-03 00:53:33 +0100

    Optimize day 02

--- day02/main.rb
+++ day02/main.rb
@@ -1,39 +1,32 @@
 # frozen_string_literal: true
 
-input = $<.read.split(/\n/)
+input = $<.each_line
 
-# Precompile regexes
+# Precompile regex
 match_red   = /(\d+) red/
 match_green = /(\d+) green/
 match_blue  = /(\d+) blue/
 
 def max_color(line, regex)
-  line.scan(regex).flatten.map(&:to_i).max
+  line.scan(regex).flatten.map(&:to_i).max || 0
 end
 
-# Part 1
-part1 = input.sum do |line|
-  red  = max_color(line, match_red)
-  next 0 if red > 12
+part1 = 0
+part2 = 0
 
-  green = max_color(line, match_green)
-  next 0 if green > 13
-
-  blue = max_color(line, match_blue)
-  next 0 if blue > 14
-
-  line.match(/^Game (\d+)/)[1].to_i
-end
-
-puts "Part 1: #{part1}"
-
-# Part 2
-part2 = input.sum do |line|
+input.each_with_index do |line, idx|
   red   = max_color(line, match_red)
   green = max_color(line, match_green)
   blue  = max_color(line, match_blue)
 
-  red * green * blue
+  part2 += red * green * blue
+
+  next if red > 12
+  next if green > 13
+  next if blue > 14
+
+  part1 += idx + 1
 end
 
+puts "Part 1: #{part1}"
 puts "Part 2: #{part2}"
